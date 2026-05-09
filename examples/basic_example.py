9d2b5c5e2028e2aa32a66fa6de6d8dc30eb439a2
Graham <kerr.graham.d@gmail.com>
2026-05-08 22:21:52 -0700

    Add DC config call

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -137,6 +137,9 @@ class BasicExample:
         for slave in self._master.slaves:
             slave.dc_sync(act=True, sync0_cycle_time=10_000_000)  # time is given in ns -> 10,000,000ns = 10ms
 
+        dc_devices = self._master.config_dc()
+        print(f"Configured DC. Found {dc_devices} compatible devices")
+
         if self._master.state_check(pysoem.SAFEOP_STATE, timeout=50_000) != pysoem.SAFEOP_STATE:
             self._master.close()
             raise BasicExampleError("not all slaves reached SAFEOP state")
